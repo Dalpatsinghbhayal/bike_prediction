f7b49d2b3d53b8ce7f004aab23c38f84e961dc3d
Dalpatsinghbhayal <113092138+Dalpatsinghbhayal@users.noreply.github.com>
2024-06-12 21:54:33 +0530

    Add files via upload

---
+++ ml.project/app.py
@@ -0,0 +1,37 @@
+from flask import Flask ,render_template ,url_for ,request
+import joblib 
+
+model = joblib.load(r'C:\Users\dalpa\OneDrive\Documents\ml.project\linear_regression.lb')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/user_data')
+def user_data():
+  return render_template('user_data.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+  if request.method == 'POST':
+    brand_name =int(request.form['brand_name'])
+    Kms_Driven = int(request.form['Kms_Driven'])
+    owner =int(request.form['owner'])
+    age= int(request.form['age'])
+    power = int(request.form['power'])
+
+    
+
+    user_data = [[owner,brand_name,Kms_Driven,age,power]]
+    pred = model.predict(user_data)[0][0]
+    prediction=str(round(pred,2))
+    return render_template('user_data.html',prediction_text=prediction)
+    
+
+
+
+if __name__=="__main__":
+  app.run(debug=True)
